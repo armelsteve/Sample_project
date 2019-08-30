921245dd87eb8819a3a40ce31aadedb11a8fb80b
Armel Steve <agansop@gmail.com>
2019-08-30 15:37:47 -0400

    First project

---
+++ rolling_dice.py
@@ -0,0 +1,19 @@
+import random
+
+def rolling_dice():
+    x = random.randint(1, 6)
+
+    if x == 1:
+        print(x)
+    if x == 2:
+        print(x)
+    if x == 3:
+        print(x)
+    if x == 4:
+        print(x)
+    if x == 5:
+        print(x)
+    if x == 6:
+        print(x)
+
+rolling_dice()
